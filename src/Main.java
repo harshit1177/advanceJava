5faeab23e05f0f6373f8db24d2af0fe35c60f8be
harshit1177 <harshit12345c@gmail.com>
2024-06-21 17:08:15 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//Collections framework
+//Exception handling
+//Multithreading
+//lambda expression and functional interfaces.
+//builder design pattern and singleton pattern.
+
+// final, this , super , static , abstract, finalize, finally, Object class
+//object class methods.
+//rememeber the heirarhical diagram of collections framework.
+
+//dynamic in nature. --> grow on demand
+
+
+
+
---
+++ src/com/java/collections/Main.java
@@ -0,0 +1,44 @@
+package com.java.collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+        ArrayList<Integer> noList = new ArrayList();
+        noList.add(10);
+        noList.add(11);
+        noList.add(12);
+        noList.add(12);
+        System.out.println(noList);
+        
+        String abc = "names2";
+        abc.length();
+        
+        int[] noArray = {1,2,3,4};
+        int length = noArray.length;
+        System.out.println(noArray[1]);
+
+        System.out.println(abc.charAt(2));
+
+        LinkedList<String> names = new LinkedList<>();
+
+        for(int i =0 ;i<noList.size();i++){
+            System.out.println(noList.get(i));
+        }
+
+
+
+
+
+    }
+}
